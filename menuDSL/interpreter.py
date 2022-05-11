40c03e61792431e295c20a4c6eb37c116c58423e
marijamilanovic <marijamil@uns.ac.rs>
2022-05-11 19:21:34 +0200

    [FeatureUpdate] modified interpreter

--- menuDSL/interpreter.py
+++ menuDSL/interpreter.py
@@ -1,10 +1,8 @@
 from os import mkdir
 from os.path import join, dirname, exists
-from textx import metamodel_from_file
+from textx import metamodel_from_file, metamodel_for_language, register_language
 from textx.export import metamodel_export, model_export_to_file, model_export
 from io import StringIO
-import psycopg2
-
 
 def get_meta_model():
     current_dir = dirname(__file__)
@@ -24,10 +22,25 @@ def export_meta_model():
 
 def export_example_model():
     my_model = get_model('example.rbt')
-    print(my_model.menuSections[0].items[0].type)
+    #print(my_model.menu_sections[0].items[0].type)
     model_export(my_model, 'example.dot')
+    return my_model
+
+class Menu:
+    def __init__(self, menu_sections=None):
+        self.menu_sections = menu_sections
+
+    def interpret(self, model):
+        #print(model.menu_sections[0].items[0].type)
+        for m in model.menu_sections:
+            #print(m.__class__.__name__)
+            #print(m.section_type)
+            for item in m.items:
+                print(item.type)
 
 
 if __name__ == "__main__":
     export_meta_model()
-    export_example_model()
+    my_model = export_example_model()
+    menu = Menu()
+    menu.interpret(my_model)
