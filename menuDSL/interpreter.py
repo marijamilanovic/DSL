3ab109be300833f38d04f7e4df69df433c609537
Maja18 <maja98dragojlovic@gmail.com>
2022-05-11 11:21:05 +0200

    [Feature] get data from database based on given filter

--- menuDSL/interpreter.py
+++ menuDSL/interpreter.py
@@ -4,7 +4,7 @@ from textx import metamodel_from_file
 from textx.export import metamodel_export, model_export_to_file, model_export
 from io import StringIO
 import psycopg2
-
+from config import config
 
 def get_meta_model():
     current_dir = dirname(__file__)
@@ -24,10 +24,56 @@ def export_meta_model():
 
 def export_example_model():
     my_model = get_model('example.rbt')
-    print(my_model.menuSections[0].items[0].type)
     model_export(my_model, 'example.dot')
 
+def get_data_from_database():
+    my_model = get_model('example.rbt')
+    for menu_section in my_model.menuSections:
+        for item in menu_section.items:
+            if menu_section.section_type == 'Food':
+                get_food_data_from_database(item.type)
+            else:
+                get_drink_data_from_database(item.type)
+
+def get_food_data_from_database(type):
+    conn = None
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        
+        cur.execute("SELECT * FROM food WHERE food_type=%(food_type)s", {'food_type': type})
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+        
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+def get_drink_data_from_database(type):
+    conn = None
+    try:
+        params = config()
+        conn = psycopg2.connect(**params)
+        cur = conn.cursor()
+        
+        cur.execute("SELECT * FROM drink WHERE drink_type=%(drink_type)s", {'drink_type': type})
+        rows = cur.fetchall()
+        for row in rows:
+            print(row)
+
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
 
 if __name__ == "__main__":
     export_meta_model()
     export_example_model()
+    get_data_from_database()
